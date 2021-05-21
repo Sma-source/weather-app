91fb4a0e06ab0ca6d0a9fb07671855104f2bd613
Sma-source <rabhi.smain@gmail.com>
2021-05-21 16:52:51 +0200

    add weather component ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React from "react";
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "Janvier",
+      "Fevrier",
+      "Mars",
+      "Avril",
+      "Mai",
+      "Juin",
+      "Juillet",
+      "Aout",
+      "Septembre",
+      "Octobre",
+      "Novembre",
+      "Décembre",
+    ];
+    let days = [
+      "Dimanche",
+      "Lundi",
+      "Mardi",
+      "Mercredi",
+      "Jeudi",
+      "Vendredi",
+      "Samedi",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <div className="App warm">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">Paris, FR</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">15°c</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
