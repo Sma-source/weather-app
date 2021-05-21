27c9d27c34f070e49cbfaf8805f7fd8900129eeb
Sma-source <rabhi.smain@gmail.com>
2021-05-21 15:35:25 +0200

    modify ui

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="App warm">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main>
     </div>
   );
 }
